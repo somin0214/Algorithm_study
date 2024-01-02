13c1510477354e33fe2acc93e85d154ab8b9351f
somin0214 <151808855+somin0214@users.noreply.github.com>
2024-01-02 16:30:46 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 39.83 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,16 +1,37 @@
+# def solution(n, m):
+#     a, b = sorted([n, m])
+#     x, y = 0, 0
+    
+#     # 최대 공약수 구하기 
+#     for i in range(1, b+1):
+#         if a % i == 0 and b % i == 0:
+#             x = i
+    
+#     # 최소 공배수 구하기 
+#     for j in range(a, a*b + 1):
+#         if j % a == 0 and j % b == 0:
+#             y = j
+#             break
+
+#     return [x, y]
+
 def solution(n, m):
-    a, b = sorted([n, m])
-    x, y = 0, 0
-    
-    # 최대 공약수 구하기 
-    for i in range(1, b+1):
-        if a % i == 0 and b % i == 0:
-            x = i
-    
-    # 최소 공배수 구하기 
-    for j in range(a, a*b + 1):
-        if j % a == 0 and j % b == 0:
-            y = j
+    answer = []
+    for i in range(max(m,n),m*n+1):
+        if i%m==0 and i%n==0:
+            answer.append(i)
             break
 
-    return [x, y]
+    if n>m:
+        while n%m!=0:
+                c=n%m
+                n,m=m,c
+        answer.append(m)
+    else:
+        while m%n!=0:
+            c=m%n
+            m,n=n,c
+        answer.append(n)
+
+    answer.sort()
+    return answer
