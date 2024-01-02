4b5de8af6d45feef3b912fd94a4bcd3abee5c4d2
somin0214 <151808855+somin0214@users.noreply.github.com>
2024-01-02 16:37:27 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,14 +1,25 @@
-def solution(s):
-    new = s.split(' ')
-    answer_list = []
-    a = ''
-    for voca in new:
-        for i in range(len(voca)):
-            if i % 2 == 0:
-                a += voca[i].upper()
-            else:
-                a += voca[i].lower()
-        answer_list.append(a)
-        a = ''
+# def solution(s):
+#     new = s.split(' ')
+#     answer_list = []
+#     a = ''
+#     for voca in new:
+#         for i in range(len(voca)):
+#             if i % 2 == 0:
+#                 a += voca[i].upper()
+#             else:
+#                 a += voca[i].lower()
+#         answer_list.append(a)
+#         a = ''
                 
-    return ' '.join(answer_list)
+#     return ' '.join(answer_list)
+def solution(s):
+    answer = []
+    for i in s.split(' '):
+        i=list(i)
+        for k in range(len(i)):
+            if k%2==0 or k==0:
+                i[k]=i[k].upper()
+            else:
+                i[k]=i[k].lower()
+        answer.append(''.join(i))
+    return ' '.join(answer)
